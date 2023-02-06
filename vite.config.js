e1454ea0d18ffe1d42e0adad6147f8f698679d09
Jeferson Camero <112217986+jefdavgar@users.noreply.github.com>
2023-02-06 18:59:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jefdavgar.github.io/proyect-rick-and-morty/",
+  base: "https://jefdavgar.github.io/project-rick-and-morty/",
   plugins: [react(), eslint()],
 });
